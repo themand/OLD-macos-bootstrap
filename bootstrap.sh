7d0ca4c84f9080d6781e8f03a6a6dfb4533c1027
themand <42844500+themand@users.noreply.github.com>
2019-03-08 16:45:12 +0100

    Removed Webstorm auto installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,6 @@ ${0%/*}/scripts/dotfiles.sh || err
 ${0%/*}/scripts/hosts.sh || err
 ${0%/*}/scripts/jq.sh || err
 ${0%/*}/scripts/chrome.sh || err
-${0%/*}/scripts/webstorm.sh || err
 
 H1 "Bootstrap finished!"
 H1 "System restart recommended"
