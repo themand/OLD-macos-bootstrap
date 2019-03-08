acbf766f0a34ee84bbeb62cb952b49be5c7db798
themand <42844500+themand@users.noreply.github.com>
2019-03-08 12:30:03 +0100

    hosts-update installed as launchd agent to run daily

--- scripts/hosts.sh
+++ scripts/hosts.sh
@@ -5,8 +5,17 @@ set -euo pipefail
 
 H1 /etc/hosts blacklist
 H2 Installing script
-mkdir -p ~/bin
-cp ${0%/*}/hosts/hosts-update.sh ~/bin/hosts-update
-H2 Downloading and installing hosts blacklist
-~/bin/hosts-update
+sudo mkdir -p /usr/local/bin
+sudo cp ${0%/*}/hosts/hosts-update.sh /usr/local/bin/hosts-update
+if [[ ! -f "/etc/hosts_permanent" ]]; then
+    sudo cp /etc/hosts /etc/hosts_permanent
+fi
 
+H2 "Scheduling daily execution via launchd (logging to /var/log/hosts-update.log)"
+if [[ $(sudo launchctl list | grep com.github.themand.macos-bootstrap.hosts-update) && -f "/Library/LaunchAgents/com.github.themand.macos-bootstrap.hosts-update.plist" ]]; then
+    H3 "Stopping already running launch agent"
+    sudo launchctl unload /Library/LaunchAgents/com.github.themand.macos-bootstrap.hosts-update.plist
+fi
+sudo cp ${0%/*}/hosts/com.github.themand.macos-bootstrap.hosts-update.plist /Library/LaunchAgents/
+sudo launchctl load /Library/LaunchAgents/com.github.themand.macos-bootstrap.hosts-update.plist
+H3 OK
--- scripts/hosts/hosts-update.sh
+++ scripts/hosts/hosts-update.sh
@@ -2,22 +2,28 @@
 
 set -euo pipefail
 
-if [ ! -f "$HOME/.hosts" ]; then
-    echo "Hosts not updated. ~/.hosts does not exists"
+if [ $(whoami) != "root" ]; then
+    (>&2 echo -e $(date +%FT%T%z)"\tERROR\thosts-update needs to be run by root")
     exit 1
 fi
 
-TMPFILE="$TMPDIR$(uuidgen)"
+if [[ ! -f "/etc/hosts_permanent" ]]; then
+    (>&2 echo -e $(date +%FT%T%z)"\tERROR\tHosts not updated. /etc/hosts_permanent does not exists")
+    exit 2
+fi
 
+TMPFILE="/tmp/hosts-update-$(uuidgen)"
 (
     echo "# ==== WARNING: Do not edit hosts, as it will be overwritten soon"
-    echo "# ==== Instead: Edit ~/.hosts file and then run ~/bin/hosts-update"
-    echo "# ---------- Contents of ~/.hosts" && \
-    cat ~/.hosts && \
+    echo "# ==== Instead: Edit /etc/hosts_permanent file and then run hosts-update"
+    echo "# ---------- Contents of /etc/hosts_permanent" && \
+    cat /etc/hosts_permanent && \
     echo "# ---------- Blacklist from https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" && \
     curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | grep -E "^0.0.0.0|^#|^$"
 ) | tee $TMPFILE > /dev/null
 
-sudo mv $TMPFILE /etc/hosts
-sudo dscacheutil -flushcache
-sudo killall -HUP mDNSResponder
+mv $TMPFILE /etc/hosts
+dscacheutil -flushcache
+killall -HUP mDNSResponder
+
+echo -e $(date +%FT%T%z)"\tOK\tHosts updated. "$(cat /etc/hosts | wc -l | xargs)" lines in /etc/hosts"
