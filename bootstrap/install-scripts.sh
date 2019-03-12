09a6071b7e723e691959b911dc5e0e1117d0bdde
themand <42844500+themand@users.noreply.github.com>
2019-03-12 22:43:02 +0100

    Bootstrap installing genusername

--- bootstrap/install-scripts.sh
+++ bootstrap/install-scripts.sh
@@ -22,8 +22,8 @@ H2 "shamir-sharing"
 SHAMIR_TMPDIR="${TMPDIR}$(uuidgen)"
 mkdir -p $SHAMIR_TMPDIR
 H3 "Downloading..."
-curl -L -o ${SHAMIR_TMPDIR}/macos-shamir-split https://github.com/themand/shamir-sharing/releases/download/v1.0.1/macos-shamir-split > /dev/null
-curl -L -o ${SHAMIR_TMPDIR}/macos-shamir-restore https://github.com/themand/shamir-sharing/releases/download/v1.0.1/macos-shamir-restore > /dev/null
+curl -sSL -o ${SHAMIR_TMPDIR}/macos-shamir-split https://github.com/themand/shamir-sharing/releases/download/v1.0.1/macos-shamir-split
+curl -sSL -o ${SHAMIR_TMPDIR}/macos-shamir-restore https://github.com/themand/shamir-sharing/releases/download/v1.0.1/macos-shamir-restore
 H3 "Verifying checksums..."
 SHAMIR_VALID_SPLIT="4503738588fdcfc4ac4b5f9c2fbf873ea6a5bdcc1941b17cf24d21a546c3ac64"
 SHAMIR_VALID_RESTORE="01aef891489658921f84f71c1f948eb95377b9c63a23d56d6bff5b0958fe6daf"
@@ -43,3 +43,25 @@ else
     H3 "macos-shamir-restore valid sha256      : ${SHAMIR_VALID_RESTORE}"
 fi
 rm -R "$SHAMIR_TMPDIR"
+
+H2 "genusername"
+USERGEN_TMPDIR="${TMPDIR}$(uuidgen)"
+mkdir -p "$USERGEN_TMPDIR"
+H3 "Downloading..."
+curl -sSL -o ${USERGEN_TMPDIR}/genusername.js https://raw.githubusercontent.com/themand/genusername/master/genusername.js
+H3 "Verifying checksum..."
+USERGEN_VALID="6992dde02d43bb03327d0f0baab77d59cae7362abafec852963ec3334005db40"
+USERGEN_SHA=$(shasum -p -a 256 $USERGEN_TMPDIR/genusername.js | cut -d' ' -f1)
+if [[ "$USERGEN_SHA" == "$USERGEN_VALID" ]]; then
+    H3 "Installing..."
+    (
+        echo '#!/usr/local/bin/node'
+        cat "$USERGEN_TMPDIR"/genusername.js
+    ) | sudo tee /usr/local/bin/genusername > /dev/null
+    sudo chmod +x /usr/local/bin/genusername
+else
+    H2WARN "Checksums invalid. Not installing. Deleting downloaded files"
+    H3 "genusername.js downloaded sha256   : ${USERGEN_SHA}"
+    H3 "genusername.js valid sha256        : ${USERGEN_VALID}"
+fi
+rm -R "$USERGEN_TMPDIR"
