35e8e83711961f530a51e7ecf9df95fea8f0e53c
themand <42844500+themand@users.noreply.github.com>
2019-03-26 12:26:40 +0100

    Upgraded shamir-sharing to 1.0.2

--- install/script-shamir-sharing.sh
+++ install/script-shamir-sharing.sh
@@ -4,14 +4,18 @@ set -euo pipefail
 . ${0%/*}/../includes/functions.sh
 
 H2 "shamir-sharing"
+SHAMIR_VER="v1.0.2"
+if [ $(curl -s https://api.github.com/repos/themand/shamir-sharing/releases/latest | jq -r ".name") != "$SHAMIR_VER" ]; then
+    H2WARN "Newer shamir-sharing version found ("$(curl -s https://api.github.com/repos/themand/shamir-sharing/releases/latest | jq -r ".name")"). Installing known $SHAMIR_VER, please check for upgrade"
+fi
 SHAMIR_TMPDIR="${TMPDIR}$(uuidgen)"
 mkdir -p $SHAMIR_TMPDIR
 H3 "Downloading..."
-curl -sSL -o ${SHAMIR_TMPDIR}/macos-shamir-split https://github.com/themand/shamir-sharing/releases/download/v1.0.1/macos-shamir-split
-curl -sSL -o ${SHAMIR_TMPDIR}/macos-shamir-restore https://github.com/themand/shamir-sharing/releases/download/v1.0.1/macos-shamir-restore
+curl -sSL -o ${SHAMIR_TMPDIR}/macos-shamir-split https://github.com/themand/shamir-sharing/releases/download/v1.0.2/macos-shamir-split
+curl -sSL -o ${SHAMIR_TMPDIR}/macos-shamir-restore https://github.com/themand/shamir-sharing/releases/download/v1.0.2/macos-shamir-restore
 H3 "Verifying checksums..."
-SHAMIR_VALID_SPLIT="4503738588fdcfc4ac4b5f9c2fbf873ea6a5bdcc1941b17cf24d21a546c3ac64"
-SHAMIR_VALID_RESTORE="01aef891489658921f84f71c1f948eb95377b9c63a23d56d6bff5b0958fe6daf"
+SHAMIR_VALID_SPLIT="13d83ab7106a3dd63b5c7fdcfde49f18d9d33a3328bb4956b900dd845ef3c778"
+SHAMIR_VALID_RESTORE="1caded9c2445fbbdcbe001e30231f586fe85e2501e1135371c120586c2936ce2"
 SHAMIR_SHA_SPLIT=$(shasum -p -a 256 $SHAMIR_TMPDIR/macos-shamir-split | cut -d' ' -f1)
 SHAMIR_SHA_RESTORE=$(shasum -p -a 256 $SHAMIR_TMPDIR/macos-shamir-restore | cut -d' ' -f1)
 if [[ "$SHAMIR_SHA_SPLIT" == "$SHAMIR_VALID_SPLIT" ]] \
